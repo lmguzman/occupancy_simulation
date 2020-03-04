bcacea1b561ae14c51813b052ff8320349ea9d68
lmguzman <guzman@zoology.ubc.ca>
2020-03-04 14:49:48 -0800

    updating script

--- single-species.R
+++ single-species.R
@@ -1,4 +1,5 @@
 setwd('~/Desktop')
+
 rm(list=ls())
 
 library(abind)
@@ -10,17 +11,21 @@ library(R2WinBUGS)
 ## ------------------------------------------------------------
 ## create data
 
-## number of sites the species is actually at
-nsite <- 50
+## number of sites the species can actually occupy
+nsite <- 100
+##
+## numer of 'known absence sites' (E.g., sites in Europe for a North
+## American bee) Change nsite.foreign from 0 to any other value. 
+nsite.foreign <- 1000
+## occupancy in each time period
+## note: we find this effect even when the occupancy is the same in both periods
+psi.1 <- 0.8
+psi.2 <- 0.8 
+## actual detectability in each time period
+p.1 <- 0.3
+p.2 <- 0.5
 ## number of visits to each site
 nvisit <- 3
-## numer of 'known absence sites' (E.g., sites in Europe for a North
-## American bee)
-nsite.foreign <- 500
-## detectability in first era
-p.1 <- 0.3
-## detectability in second era
-p.2 <- 0.5
 
 ## create occupancy matrix for nsite actually occupied sites
 X <- array(0, dim=c(nsite=nsite, nvisit=nvisit, nera=2))
@@ -29,6 +34,12 @@ X <- array(0, dim=c(nsite=nsite, nvisit=nvisit, nera=2))
 ## probabilities
 X[,,1] <- rbinom(n=length(X[,,1]), size=1, prob=p.1)
 X[,,2] <- rbinom(n=length(X[,,2]), size=1, prob=p.2)
+
+## subset to sites with at least one detection
+##
+## NOTE: this means that parameters used to simulate occupancy and
+## detectability are no longer meaningful
+X <- X[apply(X,1,sum)>0,,,drop=FALSE]
 
 ## add known zero occupancy sites
 if(nsite.foreign>0) {
@@ -68,17 +79,19 @@ my.params <- c('psi', 'p', 'Z')
 ## ------------------------------------------------------------
 ## occupancy model
 my.model <- function() {
-
+  
+  ## occupancy
+  psi ~ dunif(0,1)
+  
   for(era in 1:nera) {
-
-    ## Priors
-    psi[era] ~ dunif(0,1)
+    
+    ## detectability
     p[era]   ~ dunif(0,1)
     
     ## Likelihood
     for(site in 1:nsite) {
       ## Occurrence
-      Z[site,era] ~ dbern(psi[era])
+      Z[site,era] ~ dbern(psi)
       p.eff[site,era] <- Z[site,era] * p[era]
       for(visit in 1:nvisit) {
         ## Detection 
@@ -103,7 +116,7 @@ jags.out <- run.jags(model='model.txt',
                      method="parallel")
 
 ## ------------------------------------------------------------
-## create Z and psi sims array
+## create Z array
 sims.mat <- do.call(rbind, jags.out$mcmc)
 vars <- dimnames(sims.mat)[[2]]
 Z <- vars[grep('Z\\[', vars)]
@@ -112,7 +125,6 @@ Z.sims.array <<- array(sims.mat[,Z],
                        dim=c(iter=niter,
                              nsite=nsite,
                              nera=nera))
-psi.sims.array <- sims.mat[,vars[grep('psi\\[', vars)]]
 
 ## ------------------------------------------------------------
 ## average across MCMC iterations and then subset Z 
@@ -125,9 +137,13 @@ Z.means.sub <- Z.means.all[apply(X, 1, sum)>0,]
 ## ------------------------------------------------------------
 ## calculate estimated mean change in occupancy at occupied sites
 ## (a value other than zero is incorrect)
-mean(Z.means.sub[,1]-Z.means.sub[,2])
+mean(Z.means.sub[,2]-Z.means.sub[,1])
 
-## ------------------------------------------------------------
-## look at estimated mean occupancy in each era
-colMeans(psi.sims.array)
+## compare with mean change in occupancy from the raw data
+##
+## first drop zero sites again
+X <- X[apply(X,1,sum)>0,,,drop=FALSE]
+Z.actual <- (apply(X, c(1,3), sum)>0)*1
+diff(apply(Z.actual, 2, mean))
 
+## for large nsite.foreign, these values are the same
